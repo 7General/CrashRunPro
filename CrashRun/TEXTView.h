2440d8d704085425eb33443daff9d76d996d748a
王会洲 <wanghuizhou21@163.com>
2017-08-17 12:13:36 +0800

    ......

---
+++ CrashRun/TEXTView.h
@@ -0,0 +1,13 @@
+//
+//  TEXTView.h
+//  CrashRun
+//
+//  Created by ZZG on 17/8/17.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEXTView : UIView
+
+@end
---
+++ CrashRun/TEXTView.m
@@ -0,0 +1,14 @@
+//
+//  TEXTView.m
+//  CrashRun
+//
+//  Created by ZZG on 17/8/17.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import "TEXTView.h"
+
+@implementation TEXTView
+///ddddddd
+
+@end
